a3ed2d7c27c4ddda80287368d77539fc43e934a9
Ikaris12 <erosiannoccari@gmail.com>
2025-11-05 18:11:52 +0100

    added multiple stages

--- game/scenes/GameScene.js
+++ game/scenes/GameScene.js
@@ -15,6 +15,7 @@ export class GameScene extends Phaser.Scene {
   create() {
     if (this.score === undefined) this.score = 0;
     if (this.completion === undefined) this.completion = 0;
+    if (this.objective === undefined) this.objective = 50;
     //===GROUND===
     this.ground = this.add.tileSprite(
       this.scale.width / 2,
@@ -73,10 +74,15 @@ export class GameScene extends Phaser.Scene {
   }
 
   initUI() {
-    this.scoreText = this.add.text(this.scale.width / 2, 16, "Gemme: 0 /50", {
-      fontSize: "20px",
-      fill: "#fff",
-    });
+    this.scoreText = this.add.text(
+      this.scale.width / 2,
+      16,
+      "Gemme: 0 /" + this.objective,
+      {
+        fontSize: "20px",
+        fill: "#fff",
+      }
+    );
   }
   jump() {
     this.player.setVelocityY(JUMP_FORCE);
@@ -113,9 +119,10 @@ export class GameScene extends Phaser.Scene {
   collectGem(player, gem) {
     gem.destroy();
     this.score += 1;
-    this.scoreText.setText("Gemme: " + this.score + " /50");
-    if (this.score >= (this.completion + 1) * 3) {
+    this.scoreText.setText("Gemme: " + this.score + " /" + this.objective);
+    if (this.score >= this.objective) {
       this.completion++;
+      this.objective = 50 * (this.completion + 1);
       this.scoreText.setColor("#c8c02a");
       this.scene.start("StageCompleteScene");
     }
